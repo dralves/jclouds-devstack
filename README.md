73bd393bdc321d7f06dd879e58fb950554a95701
David Ribeiro Alves <davidralves@gmail.com>
2012-04-09 04:12:42 +0100

    added devstack statement

---
+++ README.md
@@ -0,0 +1,28 @@
+
+#Setup
+
+Have clouds virtualbox working. see: https://github.com/jclouds/jclouds/tree/master/labs/virtualbox
+
+That's it! 
+
+--------------
+
+#Booting devstack in a VM
+
+start a clojure command line:
+
+> mvn clean install clojure:repl 
+
+issue the following commands:
+
+>(use 'org.jclouds.compute2)
+>(import 'org.jclouds.openstack.devstack.Devstack)
+>(def compute (compute-service "virtualbox" "" "" :sshj :slf4j))
+>(create-nodes compute "devstack" 1 (build-template compute { :run-script (Devstack/inVm) } ))
+
+Devstack dashboard will be available at: http://new_node_ip, and is accessible with the credentials admin/password
+
+#Note
+
+Due to the fact that that addresses are assigned by DHCP there is currently no way to find out exactly which address is assigned to the vm running devstack. 
+This will be solved sortly but, typically, it will be one of the first ips of the assigned network.
---
+++ src/main/java/org/jclouds/openstack/devstack/Devstack.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jclouds.openstack.devstack;
+
+import org.jclouds.scriptbuilder.domain.OsFamily;
+import org.jclouds.scriptbuilder.domain.Statement;
+
+import com.google.common.collect.ImmutableSet;
+
+public class Devstack implements Statement {
+
+  public static Devstack inVm() {
+    return new Devstack();
+  }
+
+  public Iterable<String> functionDependencies(OsFamily family) {
+    return ImmutableSet.of();
+  }
+
+  public String render(OsFamily family) {
+    switch (family) {
+      case UNIX:
+        return "apt-get update\n" + "apt-get install -qqy git\n"
+            + "git clone https://github.com/cloudbuilders/devstack.git\n" + "cd devstack\n"
+            + "echo ADMIN_PASSWORD=password > localrc\n" + "echo MYSQL_PASSWORD=password >> localrc\n"
+            + "echo RABBIT_PASSWORD=password >> localrc\n" + "echo SERVICE_TOKEN=tokentoken >> localrc\n"
+            + "echo FLAT_INTERFACE=br100 >> localrc\n" + "./stack.sh";
+      default:
+        throw new UnsupportedOperationException("Only *nix is supported");
+    }
+  }
+}
